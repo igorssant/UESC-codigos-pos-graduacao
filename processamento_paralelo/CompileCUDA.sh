7555baadc4aa42a239fc42ec5096b63db567046d
igorSantana <31260344+igorssant@users.noreply.github.com>
2026-06-12 16:40:47 -0300

    fix: corrigindo arquivo de compilacao CUDA

--- processamento_paralelo/CompileCUDA.sh
+++ processamento_paralelo/CompileCUDA.sh
@@ -4,13 +4,13 @@ set -e
 
 compile() {
     echo "Compiling CUDA implementation...\n"
-    nvcc -o bin/CUDA -O3 -use_fast_math -arch=sm_90 -Xcompiler -Wall src/CUDA_implementation.cu src/config.c src/tensor.c -Ilib
+    nvcc -O3 -use_fast_math -arch=sm_90 -Xcompiler -fPIC -shared -o bin/cuda_c.so -O3 src/CUDA_implementation.cu
 }
 
-run() {
-    echo "Running CUDA implementation...\n"
-    ./bin/CUDA "$1" "$2"
-}
+#run() {
+#    echo "Running CUDA implementation...\n"
+#    ./bin/CUDA "$1" "$2"
+#}
 
 main() {
     if [ "$#" -lt 1 ]; then
@@ -20,11 +20,11 @@ main() {
 
     if [ "$1" = "compile" ]; then
         compile "$2"
-    elif [ "$1" = "run" ]; then
-        input_file="$2"
-        threshold="$3"
-
-        run "$input_file" "$threshold"
+    #elif [ "$1" = "run" ]; then
+    #    input_file="$2"
+    #    threshold="$3"
+    #
+    #    run "$input_file" "$threshold"
     else
         echo "Unknown arguments: $@"
     fi
